3dbc3721582fd8b5cfc6da15c789f49fb3c8d524
Gizachew-Eshetie <gizchanie@gmail.com>
2017-05-21 23:41:15 -0500

    Modified user and post services

--- JobLeads/src/service/PostService.java
+++ JobLeads/src/service/PostService.java
@@ -1,5 +1,132 @@
 package service;
 
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import dbconnection.Database;
+import dbconnection.IDatabase;
+import model.Post;
+
 public class PostService {
+	private IDatabase database = Database.getInstance();
+
+	public int savePost(Post post) {
+		int postId = 0;
+		try {
+			String sql = "INSERT INTO posts (postid, userid, post, posttype,  datecreated, datecreated " + ") VALUES ('"
+					+ post.getPostId() + "','" + post.getUserId() + "','" + post.getPost() + "','" + post.getPostType()
+					+ "','" + post.getDateCreated() + "'," + post.getDateUpdated() + "')";
+			postId = this.database.executeUpdate(sql);
+			if (postId != 0) {
+				post.setUserId(postId);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			this.database.closeConnection();
+		}
+		return postId;
+	}
+
+	public int deletePostById(int postId) {
+		int result = 0;
+		try {
+			String sql = "DELETE FROM posts WHERE postid = '" + postId + "'";
+			result = this.database.executeUpdate(sql);
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			this.database.closeConnection();
+		}
+		return result;
+	}
+
+	public int deletePostByUserId(int userId) {
+		int result = 0;
+		try {
+			String sql = "DELETE FROM posts WHERE userid = '" + userId + "'";
+			result = this.database.executeUpdate(sql);
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			this.database.closeConnection();
+		}
+		return result;
+	}
+
+	public Post getPostById(int postId) {
+		Post post = null;
+		try {
+			String sql = "SELECT * FROM posts WHERE postid = '" + postId + "' LIMIT 1";
+			ResultSet rs = this.database.executeQuery(sql);
+			if (rs.next()) {
+				post = new Post();
+				post.setPostId(rs.getInt(1));
+				post.setUserId(rs.getInt(2));
+				post.setPost(rs.getString(3));
+				post.setPostType(rs.getInt(4));
+				post.setDateCreated(rs.getDate(5));
+				post.setDateUpdated(rs.getDate(6));
+
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			this.database.closeConnection();
+		}
+		return post;
+	}
+
+	public List<Post> getPostByUserId(int userId) {
+		List<Post> posts = new ArrayList<>();
+		Post post = null;
+		try {
+			String sql = "SELECT * FROM posts WHERE userid = '" + userId + "' LIMIT 1";
+			ResultSet rs = this.database.executeQuery(sql);
+			while (rs.next()) {
+				post = new Post();
+				post.setPostId(rs.getInt(1));
+				post.setUserId(rs.getInt(2));
+				post.setPost(rs.getString(3));
+				post.setPostType(rs.getInt(4));
+				post.setDateCreated(rs.getDate(5));
+				post.setDateUpdated(rs.getDate(6));
+				posts.add(post);
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			this.database.closeConnection();
+		}
+		return posts;
+	}
+
+	public List<Post> getAllPosts() {
+		List<Post> posts = new ArrayList<>();
+		Post post = null;
+		try {
+			String sql = "SELECT * FROM posts";
+			ResultSet rs = this.database.executeQuery(sql);
+			while (rs.next()) {
+				post = new Post();
+				post.setPostId(rs.getInt(1));
+				post.setUserId(rs.getInt(2));
+				post.setPost(rs.getString(3));
+				post.setPostType(rs.getInt(4));
+				post.setDateCreated(rs.getDate(5));
+				post.setDateCreated(rs.getDate(6));
+				posts.add(post);
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			this.database.closeConnection();
+		}
+		return posts;
+	}
 
 }
--- JobLeads/src/service/UserProfileService.java
+++ JobLeads/src/service/UserProfileService.java
@@ -1,5 +1,8 @@
 package service;
 
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 import dbconnection.Database;
 import dbconnection.IDatabase;
 import model.User;
@@ -9,8 +12,144 @@ public class UserProfileService {
 
 	public int saveUser(User user) {
 		int userId = 0;
-
+		try {
+			String sql = "INSERT INTO users (userid, fullname, gender, state, city, street,zipcode, birthyear, email, password, datecreated, datecreated "
+					+ ") VALUES ('" + user.getUserId() + "','" + user.getName() + "','" + user.getGender() + "','"
+					+ user.getState() + "','" + user.getCity() + "'," + user.getStreet() + ",'" + user.getZip()
+					+ user.getBirthYear() + "','" + user.getEmail() + "'," + user.getDateCreated() + ",'"
+					+ user.getDateUpdated() + "')";
+			userId = this.database.executeUpdate(sql);
+			if (userId != 0) {
+				user.setUserId(userId);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			this.database.closeConnection();
+		}
 		return userId;
+	}
+
+	public int updateUser(User user) {
+		int result = 0;
+		try {
+			String sql = "UPDATE users SET fullname = '" + user.getName() + "'," + "gender = '" + user.getGender()
+					+ "'," + "state = '" + user.getState() + "'," + "city = '" + user.getCity() + "'," + "street = '"
+					+ user.getStreet() + "'," + "zipcode = " + user.getZip() + "," + "birthyear = '"
+					+ user.getBirthYear() + "email = '" + user.getEmail() + "'," + "password = '" + user.getPassword()
+					+ "'," + "datecreated = '" + user.getDateCreated() + "'," + "dateupdated = '"
+					+ user.getDateUpdated() + "' WHERE idGuest = " + user.getUserId();
+			result = this.database.executeUpdate(sql);
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			this.database.closeConnection();
+		}
+		return result;
+	}
+
+	public int deleteUserById(int userId) {
+		int result = 0;
+		try {
+			String sql = "DELETE FROM users WHERE userid = '" + userId + "'";
+			result = this.database.executeUpdate(sql);
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			this.database.closeConnection();
+		}
+		return result;
+	}
+
+	public User getUserById(int userId) {
+		User user = null;
+		try {
+			String sql = "SELECT * FROM users WHERE userid = '" + userId + "' LIMIT 1";
+			ResultSet rs = this.database.executeQuery(sql);
+			if (rs.next()) {
+				user = new User();
+				user.setUserId(rs.getInt(1));
+				user.setName(rs.getString(2));
+				user.setGender(rs.getString(3));
+				user.setState(rs.getString(4));
+				user.setCity(rs.getString(5));
+				user.setStreet(rs.getString(6));
+				user.setZip(rs.getInt(7));
+				user.setBirthYear(rs.getInt(8));
+				user.setEmail(rs.getString(9));
+				user.setPassword(rs.getString(10));
+				user.setDateCreated(rs.getDate(11));
+				user.setDateCreated(rs.getDate(12));
+
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			this.database.closeConnection();
+		}
+		return user;
+	}
+
+	public User getUserByEmail(String email) {
+		User user = null;
+		try {
+			String sql = "SELECT * FROM users WHERE email = '" + email + "' LIMIT 1";
+			ResultSet rs = this.database.executeQuery(sql);
+			if (rs.next()) {
+				user = new User();
+				user.setUserId(rs.getInt(1));
+				user.setName(rs.getString(2));
+				user.setGender(rs.getString(3));
+				user.setState(rs.getString(4));
+				user.setCity(rs.getString(5));
+				user.setStreet(rs.getString(6));
+				user.setZip(rs.getInt(7));
+				user.setBirthYear(rs.getInt(8));
+				user.setEmail(rs.getString(9));
+				user.setPassword(rs.getString(10));
+				user.setDateCreated(rs.getDate(11));
+				user.setDateCreated(rs.getDate(12));
+
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			this.database.closeConnection();
+		}
+		return user;
+	}
+
+	public List<User> getAllUsers() {
+		List<User> users = new ArrayList<>();
+		User user = null;
+		try {
+			String sql = "SELECT * FROM users";
+			ResultSet rs = this.database.executeQuery(sql);
+			while (rs.next()) {
+				user = new User();
+				user.setUserId(rs.getInt(1));
+				user.setName(rs.getString(2));
+				user.setGender(rs.getString(3));
+				user.setState(rs.getString(4));
+				user.setCity(rs.getString(5));
+				user.setStreet(rs.getString(6));
+				user.setZip(rs.getInt(7));
+				user.setBirthYear(rs.getInt(8));
+				user.setEmail(rs.getString(9));
+				user.setPassword(rs.getString(10));
+				user.setDateCreated(rs.getDate(11));
+				user.setDateCreated(rs.getDate(12));
+				users.add(user);
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			this.database.closeConnection();
+		}
+		return users;
 	}
 
 }
